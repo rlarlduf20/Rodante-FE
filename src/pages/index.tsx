29b54ca173204fb13b184af8294069a887ec91df
rlarlduf20 <kky8274@naver.com>
2022-07-25 16:55:24 +0900

    emotion, mui 초기설정

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Button>안녕</Button>
+    </div>
+  );
+};
+
+export default Home;
